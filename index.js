6021db25b6e6603e8f03582155956304e30ba705
alejandro <alejandro.luthi@gmail.com>
2022-09-21 14:19:59 +0200

    fix: length error removed

--- index.js
+++ index.js
@@ -12,130 +12,138 @@
 // @description 14/09/2022, 14:34:12
 // ==/UserScript==
 
-$(document).ready(function() {
-    $(document).on("click","#TSTimesheetTable_TimesheetNbr_GridView_3_0_0_input",function() {
-      waitForElm(".multilineInput-textArea").then((elm) => {
+$(document).ready(function () {
+    $(document).on("click", "#TSTimesheetTable_TimesheetNbr_GridView_3_0_0_input", function () {
+        waitForElm(".multilineInput-textArea").then((elm) => {
 
-        var state = localStorage.getItem('storedData');
-        let data = [];
-        if (state) {
-          data = [...JSON.parse(state)];
-        } else {
-          console.log("");
-        }
-
-        document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.after(createDropdown(data));
-
-        document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.after(deleteValue(data));
-
-        document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.after(bigRedButton(data));
-
-        document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.onchange = function() {getValue()};
-
-        document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.onchange = function() {
-          if (getValue() === "") {
-          console.log("is empty");
-          } else {
-          let value = getValue();
-          let res = value.replace(/^[ ]+/g, "");
-          let short = res.replace(/[ ]+$/g, "");
-          console.log(short)
-          let matches = false;
-          data.forEach(store => {
-            if (store === short) {
-              matches = true;
+            var state = localStorage.getItem('storedData');
+            let data = [];
+            if (state) {
+                data = [...JSON.parse(state)];
+            } else {
+                console.log("");
             }
-          })
 
-          if (matches != true) {
-            data.push(short);
-            if (data.length > 11) {
-            data.splice(0, 1);
-            localStorage.setItem('storedData', JSON.stringify(data));
-              createSelectFields();
-              createDeleteFields();
-          } else {
-            localStorage.setItem('storedData', JSON.stringify(data));
-            createSelectFields();
-            createDeleteFields();
-          }
-          }
-          matches = false;
-        }
+            document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.after(createDropdown(data));
 
-          document.getElementById("You're_welcome_Boris").onchange = function() {onSelect()};
-        };
+            document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.after(deleteValue(data));
 
-        document.getElementById("You're_welcome_Boris").onchange = function() {onSelect()};
-        document.getElementById("You're_welcome_Janes").onchange = function() {onDeleteSelect()};
+            document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.after(bigRedButton(data));
 
-        document.getElementById("bigRedButton").onclick = function() {
-           for (var i = 0; i < data.length; i++) {
-            data.splice(i, 1);
-            i--;
-          }
-          localStorage.setItem('storedData', JSON.stringify(data));
-          console.log(data + "test")
-          createSelectFields();
-          createDeleteFields();
-          console.log(data);
-        }
+            document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.onchange = function () {
+                getValue()
+            };
 
-        document.getElementById("You're_welcome_Janes").onchange = function() {
-          onDelete();
-        }
+            document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.onchange = function () {
+                if (getValue() === "") {
+                    console.log("is empty");
+                } else {
+                    let value = getValue();
+                    let res = value.replace(/^[ ]+/g, "");
+                    let short = res.replace(/[ ]+$/g, "");
+                    console.log(short)
+                    let matches = false;
+                    data.forEach(store => {
+                        if (store === short) {
+                            matches = true;
+                        }
+                    })
 
-        function onDelete() {
-          let value = document.getElementById("You're_welcome_Janes").value;
-          console.log(value + "v");
-          console.log(data + "d")
-            for (var i = 0; i < data.length; i++) {
-              if (data[i] === value) {
-                data.splice(i, 1);
-                console.log(data)
+                    if (matches != true) {
+                        data.push(short);
+                        if (data.length > 11) {
+                            data.splice(0, 1);
+                            localStorage.setItem('storedData', JSON.stringify(data));
+                            createSelectFields();
+                            createDeleteFields();
+                        } else {
+                            localStorage.setItem('storedData', JSON.stringify(data));
+                            createSelectFields();
+                            createDeleteFields();
+                        }
+                    }
+                    matches = false;
+                }
+
+                document.getElementById("You're_welcome_Boris").onchange = function () {
+                    onSelect()
+                };
+            };
+
+            document.getElementById("You're_welcome_Boris").onchange = function () {
+                onSelect()
+            };
+            document.getElementById("You're_welcome_Janes").onchange = function () {
+                onDeleteSelect()
+            };
+
+            document.getElementById("bigRedButton").onclick = function () {
+                for (var i = 0; i < data.length; i++) {
+                    data.splice(i, 1);
+                    i--;
+                }
                 localStorage.setItem('storedData', JSON.stringify(data));
+                console.log(data + "test")
                 createSelectFields();
                 createDeleteFields();
                 console.log(data);
-              }
             }
-        }
 
-        function createSelectFields() {
-          for (var i = 0; data.length - 1> i; i++) {
-          document.getElementById("select").remove();
-        }
+            document.getElementById("You're_welcome_Janes").onchange = function () {
+                onDelete();
+            }
 
-          for (var i = 0; i < data.length; i++) {
-          option = document.createElement("option");
-          option.value = data[i];
-          option.text = data[i];
-          option.id = "select"
-          document.getElementById("You're_welcome_Boris").appendChild(option);
-        }
-      }
+            function onDelete() {
+                let value = document.getElementById("You're_welcome_Janes").value;
+                console.log(value + "v");
+                console.log(data + "d")
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i] === value) {
+                        data.splice(i, 1);
+                        console.log(data)
+                        localStorage.setItem('storedData', JSON.stringify(data));
+                        createSelectFields();
+                        createDeleteFields();
+                        console.log(data);
+                    }
+                }
+            }
 
-      function createDeleteFields() {
-      console.log("delete");
-      var getField = document.getElementById("delete");
-      for (var i = 0; getField.length > i; i++) {
-          if (getField.options[i].value === document.getElementById("You're_welcome_Janes").value);
-          getField.remove(i);
-      }
+            function createSelectFields() {
+                for (var i = 0; data.length - 1 > i; i++) {
+                    document.getElementById("select").remove();
+                }
 
-      console.log(data)
+                for (var i = 0; i < data.length; i++) {
+                    option = document.createElement("option");
+                    option.value = data[i];
+                    option.text = data[i];
+                    option.id = "select"
+                    document.getElementById("You're_welcome_Boris").appendChild(option);
+                }
+            }
 
-      for (var i = 0; i < data.length; i++) {
-        option = document.createElement("option");
-        option.value = data[i];
-        option.text = data[i];
-        option.id = "delete";
-        console.log(i)
-        document.getElementById("You're_welcome_Janes").appendChild(option);
-      }
-  //document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.after(deleteValue(data));
-    }
-      })
+            function createDeleteFields() {
+                console.log("delete");
+
+                for (var i = 0; data.length - 1  > i; i++) {
+                    document.getElementById("delete").remove();
+                }
+
+                console.log(data)
+
+                for (var i = 0; i < data.length; i++) {
+
+                    option = document.createElement("option");
+                    option.value = data[i];
+                    option.text = data[i];
+                    option.id = "delete";
+                    console.log(i)
+                    document.getElementById("You're_welcome_Janes").appendChild(option);
+                }
+                //document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.after(deleteValue(data));
+            }
+        })
     });
 });
 
@@ -160,70 +168,70 @@ function waitForElm(selector) {
 }
 
 function createDropdown(data, create, update) {
-  var select = document.createElement("select");
-        select.id = "You're_welcome_Boris";
+    var select = document.createElement("select");
+    select.id = "You're_welcome_Boris";
 
-        var option = document.createElement("option");
-        option.setAttribute("selected", "");
-        option.setAttribute("disabled", "");
-        option.text = "Select";
+    var option = document.createElement("option");
+    option.setAttribute("selected", "");
+    option.setAttribute("disabled", "");
+    option.text = "Select";
 
-        select.appendChild(option);
+    select.appendChild(option);
 
-        for( var i = 0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
         option = document.createElement("option");
         option.value = data[i];
         option.text = data[i];
         option.id = "select"
         select.appendChild(option);
-        }
-  return select
+    }
+    return select
 }
 
 function bigRedButton(data) {
-  var button = document.createElement("button");
-  button.innerHTML = "Delete all";
-  button.name = "delete";
-  button.id = "bigRedButton";
+    var button = document.createElement("button");
+    button.innerHTML = "Delete all";
+    button.name = "delete";
+    button.id = "bigRedButton";
 
-  button.onclick = function() {
-    alert("Data has been deleted")
-  }
-  return button
+    button.onclick = function () {
+        alert("Data has been deleted")
+    }
+    return button
 }
 
 function deleteValue(data) {
-  var deleteSelect = document.createElement("select");
-  deleteSelect.id = "You're_welcome_Janes";
+    var deleteSelect = document.createElement("select");
+    deleteSelect.id = "You're_welcome_Janes";
 
-  var option = document.createElement("option");
-  option.setAttribute("selected", "");
-  option.setAttribute("disabled", "");
-  option.text = "Delete Value";
-  deleteSelect.appendChild(option);
-  for (var i = 0; i < data.length; i++) {
-    option = document.createElement("option");
-    option.value = data[i];
-    option.text = data[i];
-    option.id = "delete"
+    var option = document.createElement("option");
+    option.setAttribute("selected", "");
+    option.setAttribute("disabled", "");
+    option.text = "Delete Value";
     deleteSelect.appendChild(option);
-  }
-  return deleteSelect;
+    for (var i = 0; i < data.length; i++) {
+        option = document.createElement("option");
+        option.value = data[i];
+        option.text = data[i];
+        option.id = "delete"
+        deleteSelect.appendChild(option);
+    }
+    return deleteSelect;
 }
 
 function getValue() {
-  var value = document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.value;
-  console.log(value)
-  return value;
+    var value = document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.value;
+    console.log(value)
+    return value;
 }
 
 function onSelect() {
-  let value = document.getElementById("You're_welcome_Boris").value
-  document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.value = value;
+    let value = document.getElementById("You're_welcome_Boris").value
+    document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.value = value;
 }
 
 function onDeleteSelect() {
-  let value = document.getElementById("You're_welcome_Janes").value
-  console.log(value + " from se");
-  //document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.value = value;
+    let value = document.getElementById("You're_welcome_Janes").value
+    console.log(value + " from se");
+    //document.getElementsByClassName("multilineInput-textArea").TSTimesheetLineWeek_ExternalComments.value = value;
 }
